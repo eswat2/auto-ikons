de762ddfe524902572bb607e32008a69a327ca76
Richard Hess <eswat2@gmail.com>
2020-03-28 11:33:34 -0700

    >> updating packages

--- rollup.config.js
+++ rollup.config.js
@@ -14,11 +14,13 @@ export default {
     {
       file: pkg.main,
       format: 'cjs',
+      exports: 'named',
       sourcemap: true,
     },
     {
       file: pkg.module,
       format: 'es',
+      exports: 'named',
       sourcemap: true,
     },
   ],
